6420f4516041839b18fccd92188cdbba906d8242
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-14 18:58:52 +0100

    create .env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import ProductCard from "./Components/Cards/ProductCard";
 
 function App() {
+  const url = process.env.REACT_APP_API_ENPOINT;
+
+  console.log(url);
   return (
     <div className="App">
       <ProductCard />
