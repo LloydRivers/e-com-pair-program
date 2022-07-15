7799ff4b90af759af545fd5efc4f0c3160ae55fd
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-15 19:35:10 +0100

    setup reducers

--- frontend/src/Redux/app/store.js
+++ frontend/src/Redux/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsReducer from "../slices/ProductSlices/productsSlice";
 
-const store = configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
 });
