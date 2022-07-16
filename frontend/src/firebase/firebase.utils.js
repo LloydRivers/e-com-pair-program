6bd0aa72b33fc8851fef34961807bb36db15a67b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-07-16 14:39:46 +0300

    added my firebase api keys to my .enviroment variables

--- frontend/src/firebase/firebase.utils.js
+++ frontend/src/firebase/firebase.utils.js
@@ -28,13 +28,15 @@ import {
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxd8emKbIkN5jGFiZThMXt9xm3BtT3QxE",
-    authDomain: "ecom-pair-programing.firebaseapp.com",
-    projectId: "ecom-pair-programing",
-    storageBucket: "ecom-pair-programing.appspot.com",
-    messagingSenderId: "340769556198",
-    appId: "1:340769556198:web:2df445272ba30b9b265d27"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
+
+
 
 
 
