442ecf01eb931bed5b92c1b53ee2f448550e2b68
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-19 18:56:20 +0100

    made nav responsive

--- frontend/src/Components/Cards/ProductCard.jsx
+++ frontend/src/Components/Cards/ProductCard.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { addProduct } from "../../Redux/slices/ProductSlices/productSlice";
 import "../../Styles/productCard.css";
 const ProductCard = ({ product }) => {
+  const [liked, setLiked] = useState(false);
   const dispatch = useDispatch();
 
   const addProductToDetailsPage = () => {
     console.log("addProductToDetailsPage");
     dispatch(addProduct(product));
   };
-
+  const handleLike = () => {
+    setLiked(!liked);
+  };
   return (
     <div className="product-card">
       <div className="badge">Hot</div>
       <div className="imageContainer">
-        <img className="img" src={product.image} alt="" />
+        <Link onClick={addProductToDetailsPage} to={`/details/${product.id}`}>
+          <img className="img" src={product.image} alt="" />
+        </Link>
       </div>
       <div className="product-details">
         <span className="product-catagory">{product.category}</span>
@@ -26,12 +31,13 @@ const ProductCard = ({ product }) => {
             <small>${product.price}</small>
           </div>
           <div className="product-links">
-            <Link
-              onClick={addProductToDetailsPage}
-              to={`/details/${product.id}`}
-            >
-              <i className="fa fa-heart"></i>
-            </Link>
+            <a>
+              <i
+                onClick={handleLike}
+                style={{ color: liked ? "red" : "black" }}
+                className="fa fa-heart"
+              ></i>
+            </a>
             <a>
               <i className="fa fa-shopping-cart"></i>
             </a>
--- frontend/src/Routes/Navigation/Navigation.styles.js
+++ frontend/src/Routes/Navigation/Navigation.styles.js
@@ -1,32 +1,32 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const NavigationContainer = styled.div`
   height: 70px;
-  width: 100%;
+  max-width: 95%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
-`
+  align-items: center;
+`;
 
 export const LogoContainer = styled(Link)`
-    height: 100%;
-    width: 70px;
-   
-`
+  height: 100%;
+  width: 70px;
+`;
 export const NavLinkContainer = styled.div`
-    width: 50%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
 
 export const ImageContainer = styled.img`
-width:100px ;
-height:100px ;
-`
+  width: 100px;
+  height: 100px;
+`;
 export const NavLink = styled(Link)`
-padding: 10px 15px;
-      cursor: pointer;
-`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
