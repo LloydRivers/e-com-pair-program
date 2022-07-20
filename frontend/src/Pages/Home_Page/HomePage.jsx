573a4f36c55c9f8779fbcb06ead7db097cfd85e3
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-07-20 14:58:12 +0300

    search functionality still cannot filter out other categories.

--- frontend/src/Pages/Home_Page/HomePage.jsx
+++ frontend/src/Pages/Home_Page/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ProductCard } from "../../Components";
 import { fetchProducts } from "../../Redux/slices/ProductSlices/productsSlice";
@@ -17,34 +17,33 @@ const HomePage = () => {
   const dispatch = useDispatch();
 
   const [searchText, setSearchText] = useState("")
-const handleSearch = (e) => (setSearchText(e.target.value))
-const FilteredTitle = products?.filter((product) => (product.category.toLowerCase().includes(searchText.toLowerCase())));
+  const handleSearch = (e) => (setSearchText(e.target.value))
+  const FilteredTitle = products?.filter((product) => (product.title.toLowerCase().includes(searchText.toLowerCase())))
 
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
-  return (
-    <>
-    <Search handleChange={handleSearch}/>
+useEffect(() => {
+  dispatch(fetchProducts());
+}, []);
+return (
+  <>
+    <Search handleChange={handleSearch} />
     <HomepageContainer>
-        <Section
-          data={getCategory(FilteredTitle, "Men's Clothing")}
-          title={FilteredTitle.length > 0 ? "Men's Clothing" : null }
-        />
-        <Section data={getCategory(FilteredTitle, "Jewelery")} title={FilteredTitle.length > 0 ? "Jewelery" : null} />
-        <Section
-          data={getCategory(FilteredTitle, "Electronics")}
-          title={FilteredTitle.length > 0 ? "Electronics" : null}
-        />
-        <Section
-          data={getCategory(FilteredTitle, "Women's clothing")}
-          title={FilteredTitle.length > 0 ? "Women's clothing": null}
-        />
+      <Section
+        data={getCategory(FilteredTitle, "Men's Clothing")}
+        title={FilteredTitle ? "Men's Clothing" : null}
+      />
+      <Section data={getCategory(FilteredTitle, "Jewelery")} title={FilteredTitle > 0 ? "Jewelery" : null} />
+      <Section
+        data={getCategory(FilteredTitle, "Electronics")}
+        title={FilteredTitle > 0 ? "Electronics" : null}
+      />
+      <Section
+        data={getCategory(FilteredTitle, "Women's clothing")}
+        title={FilteredTitle > 0 ? "Women's clothing" : null}
+      />
     </HomepageContainer>
-     
-    </>
-  );
+
+  </>
+);
 };
 
 export default HomePage;
