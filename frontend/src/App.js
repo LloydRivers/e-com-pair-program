454edbf538b83a224b2e2e6dbd4ab2c20a0f7822
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-07-21 07:30:07 +0100

    add cartpage route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,48 @@
-import  { useEffect } from "react";
-import {  Route, Routes ,Navigate } from "react-router-dom";
-import {useSelector, useDispatch} from 'react-redux'
-import { HomePage, CheckoutPage, DetailsPage, CartPage, AuthenticationPage } from './Pages'
+import { useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  HomePage,
+  CheckoutPage,
+  DetailsPage,
+  CartPage,
+  AuthenticationPage,
+} from "./Pages";
 import Navigation from "./Routes/Navigation/Navigation.component";
-import { onAuthStateChanged, getAuth} from 'firebase/auth'
-import {currentUser, logIn, logOut} from './Redux/slices/authSlice/authSlice'
-
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { currentUser, logIn, logOut } from "./Redux/slices/authSlice/authSlice";
 
 function App() {
-
   const auth = getAuth();
-  const user = useSelector(currentUser)
+  const user = useSelector(currentUser);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     onAuthStateChanged(auth, (userAuth) => {
-      if (userAuth){
+      if (userAuth) {
         dispatch(
           logIn({
-            email:userAuth.email,
-            uid:userAuth.uid,
- 
+            email: userAuth.email,
+            uid: userAuth.uid,
           })
-        )
-      }else{
-        dispatch(logOut())
+        );
+      } else {
+        dispatch(logOut());
       }
-    })
-
-  }, [auth, dispatch])
+    });
+  }, [auth, dispatch]);
   return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<HomePage />} />
 
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-     
-          <Route index={true} element={<HomePage />} />
-    
         <Route path="/sign-in" element={<AuthenticationPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/details/:id" element={<DetailsPage />} />
-       
-</Route>   
-</Routes>
-
-  )
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
+      </Route>
+    </Routes>
+  );
 }
-
 
 export default App;
